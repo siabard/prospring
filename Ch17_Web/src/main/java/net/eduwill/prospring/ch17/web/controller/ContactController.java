ea81f5660759e25963e53b6de15b834db19c5989
Ian Jang <siabard@gmail.com>
2014-12-11 18:57:41 +0900

    View 페이지 작성.

--- Ch17_Web/src/main/java/net/eduwill/prospring/ch17/web/controller/ContactController.java
+++ Ch17_Web/src/main/java/net/eduwill/prospring/ch17/web/controller/ContactController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,5 +34,13 @@ public class ContactController {
 		
 		logger.info("No. of contacts:" + contacts.size());
 		return "contacts/list";
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public String show(@PathVariable("id") Long id, Model uiModel) {
+		Contact contact = contactService.findById(id);
+		uiModel.addAttribute("contact", contact);
+		
+		return "contacts/view";
 	}
 }
